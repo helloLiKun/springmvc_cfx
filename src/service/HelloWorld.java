414324cd2f561f4da2f80b441ae578ae70f3ddff
liKun <13923813589@163.com>
2018-02-06 17:47:03 +0800

    Initial commit

---
+++ src/service/HelloWorld.java
@@ -0,0 +1,11 @@
+package service;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+/**
+ * Created by likun on 2018/2/5 0005.
+ */
+@WebService
+public interface HelloWorld {
+    String sayHi(@WebParam String name);
+}
