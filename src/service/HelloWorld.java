e776ac4aa84154b462245a102b8168fafa1e6cdd
liKun <13923813589@163.com>
2018-02-07 14:32:27 +0800

    2018_2_7_1432

--- src/service/HelloWorld.java
+++ src/service/HelloWorld.java
@@ -1,5 +1,6 @@
 package service;
 
+import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 /**
@@ -7,5 +8,6 @@ import javax.jws.WebService;
  */
 @WebService
 public interface HelloWorld {
+    @WebMethod
     String sayHi(@WebParam String name);
 }
